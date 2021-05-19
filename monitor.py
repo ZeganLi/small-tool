4c159c6a8626de0187ce3512fe689098948fc561
mrliz <mr.lizegan@outlook.com>
2021-05-19 18:16:59 +0800

    去除警告信息

--- monitor.py
+++ monitor.py
@@ -5,6 +5,7 @@ import time
 from openpyxl import Workbook
 import _thread as thread
 import os.path
+from urllib3 import disable_warnings
 #获取当前时间
 def get_current_time():
     current_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
@@ -27,16 +28,19 @@ def start_monitor():
         join = os.path.join(os.getcwd(), '监控日志', time.strftime('%Y-%m-%d', time.localtime(time.time())) + '.txt')
         log_file = open(join, 'a+')
         log_file.writelines("读取到" + str(len(re_list)) + "个URL地址。" + "开始对其进行监控" + '\n')
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"}
         while True:
             content = ""
             for re in re_list:
                 replace_re = re.replace('\n', '')
                 try:
-                    r = requests.get(replace_re,timeout=5)
+                    r = requests.get(replace_re,headers=headers,timeout=5,verify=False)
                     content = log_content(r.status_code, replace_re)
                     log_file.write(content)
                     log_file.flush()
-                except:
+                except Exception as e:
+                    # print(e)
                     content = log_content("连接失败！",replace_re)
                     log_file.write(content)
                     log_file.flush()
@@ -57,5 +61,6 @@ def create_dir():
 
 
 if __name__ == '__main__':
+    disable_warnings()
     print("监控工具已启动，请查看日志文件。。。")
     start_monitor()
